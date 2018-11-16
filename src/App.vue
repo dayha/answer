2582325ebda9e56d1a32aed69b92de6b2dc6f46a
liu <361146992@qq.com>
2018-11-16 17:37:13 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      logo: true
+    };
+  }
+};
+</script>
+<style lang="less">
+@import url(css/style.css);
+@import url(css/reset.css);
+@import url(css/common.css);
+@import url(css/theme-custom.css);
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background: #fff;
+  position: relative;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <div id="btn" @click='jump_about' v-if="logo==false">
+      登录
+      <!-- <router-link to="/about"></router-link> -->
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      logo: false
+    };
+  },
+  methods: {
+    jump_about() {
+      this.logo = true;
+      // 字符串
+      this.$router.push("/about");
+
+      // 对象
+      // this.$router.push({ path: '/home/first' })
+
+      // 命名的路由
+      // this.$router.push({ name: 'home', params: { userId: wise }})
+    }
+  }
+};
+</script>
+<style lang="less">
+#btn {
+  width: 300px;
+  height: 30px;
+  background: aquamarine;
+  margin: 0 auto;
+  line-height: 30px;
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  text-align: center;
+  a {
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    line-height: 30px;
+    font-size: 18px;
+    color: #fff;
+    font-weight: bold;
+  }
+}
+</style>
